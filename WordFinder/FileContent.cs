4e3c6ca3e0ef62b22569d34227658467b1345f72
Chiara <chiarabolgiani@zentis.nl>
2022-12-01 18:07:26 +0100

    count word occurrence in files implemented

---
+++ WordFinder/FileContent.cs
@@ -0,0 +1,11 @@
+﻿namespace WordFinder
+{
+    public class FileContent
+    {
+        public Dictionary<string, int> wordsOccurrence;
+        public FileContent()
+        {
+            wordsOccurrence = new Dictionary<string, int>();
+        }
+    }
+}
---
+++ WordFinder/WordCounter/WordCounterHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace WordFinder.WordCounter
+{
+    public static class WordCounterHelper
+    {
+        public static IEnumerable<string> RemovePunctuation(this IEnumerable<string> source)
+        {
+            foreach (var item in source)
+                yield return new string(item.Where(c => !char.IsPunctuation(c)).ToArray());
+        }
+
+        public static IEnumerable<string> ToLower(this IEnumerable<string> source)
+        {
+            foreach (var item in source)
+                yield return item.ToLower();
+        }
+    }
+}
