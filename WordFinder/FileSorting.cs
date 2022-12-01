5f63e5634d3c331a9311aef852ca9e8b912844ab
Chiara <chiarabolgiani@zentis.nl>
2022-12-01 18:33:42 +0100

    file sorting strategy implemented

---
+++ WordFinder/FileSorting.cs
@@ -0,0 +1,21 @@
+﻿using WordFinder.SortingStrategies;
+
+namespace WordFinder
+{
+    public class FileSorting
+    {
+        private ISortingStategy _strategy;
+        public FileSorting(ISortingStategy strategy)
+        {
+            _strategy = strategy;
+        }
+        public void SetStrategy(ISortingStategy strategy)
+        {
+            _strategy = strategy;
+        }
+        public IEnumerable<KeyValuePair<string, int>> OrderFiles(IEnumerable<KeyValuePair<string, int>> files)
+        {
+            return _strategy.OrderFiles(files);
+        }
+    }
+}
---
+++ WordFinder/SortingStrategies/OrderByDescending.cs
@@ -0,0 +1,10 @@
+﻿namespace WordFinder.SortingStrategies
+{
+    public class OrderByDescending : ISortingStategy
+    {
+        public IEnumerable<KeyValuePair<string, int>> OrderFiles(IEnumerable<KeyValuePair<string, int>> files)
+        {
+            return files.OrderByDescending(x => x.Value);
+        }
+    }
+}
